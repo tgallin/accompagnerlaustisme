90764c6ae2bc4ecb2e996e40fedbfbb8d9641095
Thomas Gallin <thomas.gallin@gmail.com>
2017-02-02 09:50:46 +0000

    increased content font size and toggled menu when item clicked

--- app/containers/Footer.jsx
+++ app/containers/Footer.jsx
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 import styles from '../css/components/footer';
 
 var ie10WorkaroundUrl = require("file-loader?name=js/[name].[ext]!../js/ie10-viewport-bug-workaround.js");
+var customBootstrap = require("file-loader?name=js/[name].[ext]!../js/custom-bootstrap.js");
 
 const cx = classNames.bind(styles);
 
@@ -21,6 +22,7 @@ const Footer = () => {
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src={ie10WorkaroundUrl}></script>
+        <script src={customBootstrap}></script>
     </div>
     );
 };
---
+++ app/js/custom-bootstrap.js
@@ -0,0 +1,7 @@
+(function () {
+    $(document).on('click','.navbar-collapse.in',function(e) {
+      if( $(e.target).is('a') ) {
+          $(this).collapse('hide');
+      };
+  });
+})();
