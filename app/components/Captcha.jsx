6c556ddcb473c024c3099f204423f44a90c81383
Thomas Gallin <thomas.gallin@gmail.com>
2017-02-14 10:58:59 +0000

    changed recaptcha size to compact

--- app/components/Captcha.jsx
+++ app/components/Captcha.jsx
@@ -9,6 +9,7 @@ const Captcha = (props) => (
       <div className="col-sm-10">
           <ReCAPTCHA
           sitekey={recaptcha.siteKey}
+          size='compact'
           onChange={response => props.input.onChange(response)}
         />
         {props.meta.touched && props.meta.error && <div className="text-danger">{props.meta.error}</div>}
