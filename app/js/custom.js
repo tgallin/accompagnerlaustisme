adc3dccb6958106c66d90a7219542fab14a9956b
Thomas Gallin <thomas.gallin@gmail.com>
2017-05-29 15:05:05 +0000

    added a findIndex polyfill method

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,11 +1,17 @@
 (function () {
-    $(document).on('click','.navbar-collapse.in',function(e) {
-      if( $(e.target).is('a') || $(e.target).is('i') || $(e.target).is('span') || $(e.target).is('img') ) {
-          $(this).collapse('hide');
-      };
+  $(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') || $(e.target).is('i') || $(e.target).is('span') || $(e.target).is('img') ) {
+        $(this).collapse('hide');
+    };
+  });
+  $(document).on('click', '.navbar-brand', function (e) {
+    var navbar = $('.navbar-collapse.in');
+    if (navbar) {
+      navbar.collapse('hide');
+    };
   });
 })();
 
 window.recaptchaOptions = {
   lang: 'fr'
-}
+};
