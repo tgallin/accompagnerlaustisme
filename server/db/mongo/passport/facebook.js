581735325994f7982ec0f0b20d674db9e6fee2c0
Thomas Gallin <thomas.gallin@gmail.com>
2017-08-24 21:06:46 +0000

    updated facebook cdn profile image url at each authentication

--- server/db/mongo/passport/facebook.js
+++ server/db/mongo/passport/facebook.js
@@ -4,29 +4,35 @@ import User from '../models/user';
 export default (req, accessToken, refreshToken, profile, done) => {
   
   return User.findOne({ facebook: profile.id }, (findByFacebookIdErr, existingUser) => {
-    if (existingUser) return done(null, existingUser);
-    return User.findOne({ email: profile._json.email }, (findByEmailErr, existingEmailUser) => {
-      if (existingEmailUser) {
-        
-        existingEmailUser.facebook = profile.id;
-        existingEmailUser.tokens.push({ kind: 'facebook', accessToken });
-        existingEmailUser.profile.gender = existingEmailUser.profile.gender || profile._json.gender;
-        existingEmailUser.profile.picture = (profile.photos ? profile.photos[0].value : '');
-        return existingEmailUser.save((err) => {
-          done(err, existingEmailUser, { message: 'Le compte Facebook a été lié.' });
-        });
-      }
-      const user = new User();
-      user.email = profile._json.email;
-      user.facebook = profile.id;
-      user.tokens.push({ kind: 'facebook', accessToken });
-      user.profile.displayName = profile.displayName;
-      user.profile.gender = profile._json.gender;
-      user.profile.picture = profile.photos ? profile.photos[0].value : '';
-      return user.save((err) => {
-        done(err, user);
+    if (existingUser) {
+      existingUser.profile.picture = (profile.photos ? profile.photos[0].value : '');
+      return existingUser.save((err) => {
+        done(err, existingUser);
       });
-    });
+    } else {
+      return User.findOne({ email: profile._json.email }, (findByEmailErr, existingEmailUser) => {
+        if (existingEmailUser) {
+          
+          existingEmailUser.facebook = profile.id;
+          existingEmailUser.tokens.push({ kind: 'facebook', accessToken });
+          existingEmailUser.profile.gender = existingEmailUser.profile.gender || profile._json.gender;
+          existingEmailUser.profile.picture = (profile.photos ? profile.photos[0].value : '');
+          return existingEmailUser.save((err) => {
+            done(err, existingEmailUser, { message: 'Le compte Facebook a été lié.' });
+          });
+        }
+        const user = new User();
+        user.email = profile._json.email;
+        user.facebook = profile.id;
+        user.tokens.push({ kind: 'facebook', accessToken });
+        user.profile.displayName = profile.displayName;
+        user.profile.gender = profile._json.gender;
+        user.profile.picture = profile.photos ? profile.photos[0].value : '';
+        return user.save((err) => {
+          done(err, user);
+        });
+      });
+    }
   });
 };
 /* eslint-enable no-param-reassign */
--- server/db/mongo/passport/google.js
+++ server/db/mongo/passport/google.js
@@ -3,29 +3,35 @@ import User from '../models/user';
 /* eslint-disable no-param-reassign */
 export default (req, accessToken, refreshToken, profile, done) => {
   return User.findOne({ google: profile.id }, (findByGoogleIdErr, existingUser) => {
-    if (existingUser) return done(null, existingUser);
-    return User.findOne({ email: profile._json.emails[0].value }, (findByEmailErr, existingEmailUser) => {
-      if (existingEmailUser) {
-        
-        existingEmailUser.google = profile.id;
-        existingEmailUser.tokens.push({ kind: 'google', accessToken });
-        existingEmailUser.profile.gender = existingEmailUser.profile.gender || profile._json.gender;
-        existingEmailUser.profile.picture = profile._json.picture;
-        return existingEmailUser.save((err) => {
-          done(err, existingEmailUser, { message: 'Le compte Google a été lié.' });
+    if (existingUser) {
+     existingUser.profile.picture = profile._json.picture;
+     return existingUser.save((err) => {
+            done(err, existingUser);
+          });
+    } else {
+      return User.findOne({ email: profile._json.emails[0].value }, (findByEmailErr, existingEmailUser) => {
+        if (existingEmailUser) {
+          
+          existingEmailUser.google = profile.id;
+          existingEmailUser.tokens.push({ kind: 'google', accessToken });
+          existingEmailUser.profile.gender = existingEmailUser.profile.gender || profile._json.gender;
+          existingEmailUser.profile.picture = profile._json.picture;
+          return existingEmailUser.save((err) => {
+            done(err, existingEmailUser, { message: 'Le compte Google a été lié.' });
+          });
+        }
+        const user = new User();
+        user.email = profile._json.emails[0].value;
+        user.google = profile.id;
+        user.tokens.push({ kind: 'google', accessToken });
+        user.profile.displayName = profile.displayName;
+        user.profile.gender = profile._json.gender;
+        user.profile.picture = profile._json.picture;
+        return user.save((err) => {
+          done(err, user);
         });
-      }
-      const user = new User();
-      user.email = profile._json.emails[0].value;
-      user.google = profile.id;
-      user.tokens.push({ kind: 'google', accessToken });
-      user.profile.displayName = profile.displayName;
-      user.profile.gender = profile._json.gender;
-      user.profile.picture = profile._json.picture;
-      return user.save((err) => {
-        done(err, user);
       });
-    });
+    }
   });
 };
 /* eslint-enable no-param-reassign */
